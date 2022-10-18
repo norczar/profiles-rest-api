d240b750ae58b007653cab677575856116637acd
Norbert Czarnota <czarny-s@o2.pl>
2022-10-18 20:38:02 +0200

    Added custom user model, did migration of models.

--- composeexample/settings.py
+++ composeexample/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framewok.authtoken',
-    'profile_api',
+    'rest_framework.authtoken',
+    'profiles_api',
 
 ]
 
@@ -129,3 +129,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'profiles_api.UserProfile'
