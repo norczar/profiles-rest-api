473ea9bed35cc812a888d02b675da98862055638
Norbert Czarnota <czarny-s@o2.pl>
2022-10-22 22:14:06 +0200

    Fixing issues

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -5,7 +5,7 @@ set -e
 # TODO: Set to URL of git repo.
 PROJECT_GIT_URL='https://github.com/norczar/profiles-rest-api.git'
 
-PROJECT_BASE_PATH='/usr/local/apps'
+PROJECT_BASE_PATH='/usr/local/apps/'
 
 POSTGRES_NAME=postgres
 POSTGRES_USER=postgres
@@ -22,20 +22,20 @@ mkdir -p $PROJECT_BASE_PATH
 git clone $PROJECT_GIT_URL $PROJECT_BASE_PATH
 
 # Run migrations and collectstatic
-cd $PROJECT_BASE_PATH
+cd $PROJECT_BASE_PATH/profiles-rest-api
 python manage.py migrate
 python manage.py collectstatic --noinput
 
 # Configure supervisor
-cp $PROJECT_BASE_PATH/deploy/supervisor_profiles_api.conf /etc/supervisor/conf.d/profiles_api.conf
-# supervisorctl reread
-# supervisorctl update
-# supervisorctl restart profiles_api
+cp $PROJECT_BASE_PATH/profiles-rest-api/deploy/supervisor_profiles_api.conf /etc/supervisor/conf.d/profiles_api.conf
+supervisorctl reread
+supervisorctl update
+supervisorctl restart profiles_api
 
-# # Configure nginx
-# cp $PROJECT_BASE_PATH/deploy/nginx_profiles_api.conf /etc/nginx/sites-available/profiles_api.conf
-# rm /etc/nginx/sites-enabled/default
-# ln -s /etc/nginx/sites-available/profiles_api.conf /etc/nginx/sites-enabled/profiles_api.conf
-# nginx reload
+# Configure nginx
+cp $PROJECT_BASE_PATH/deploy/nginx_profiles_api.conf /etc/nginx/sites-available/profiles_api.conf
+rm /etc/nginx/sites-enabled/default
+ln -s /etc/nginx/sites-available/profiles_api.conf /etc/nginx/sites-enabled/profiles_api.conf
+nginx reload
 
 echo "DONE! :)"
