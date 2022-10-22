9af656981a897cb45a38803454e0bc817cdc7815
Norbert Czarnota <czarny-s@o2.pl>
2022-10-22 22:32:20 +0200

    debugging

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -23,20 +23,19 @@ git clone $PROJECT_GIT_URL $PROJECT_BASE_PATH
 
 # Run migrations and collectstatic
 cd $PROJECT_BASE_PATH
-ls
 python manage.py migrate
 python manage.py collectstatic --noinput
 
-# Configure supervisor
-cp $PROJECT_BASE_PATH/deploy/supervisor_profiles_api.conf /etc/supervisor/conf.d/profiles_api.conf
-supervisorctl reread
-supervisorctl update
-supervisorctl restart profiles_api
+# # Configure supervisor
+# cp $PROJECT_BASE_PATH/deploy/supervisor_profiles_api.conf /etc/supervisor/conf.d/profiles_api.conf
+# supervisorctl reread
+# supervisorctl update
+# supervisorctl restart profiles_api
 
-# Configure nginx
-cp $PROJECT_BASE_PATH/deploy/nginx_profiles_api.conf /etc/nginx/sites-available/profiles_api.conf
-rm /etc/nginx/sites-enabled/default
-ln -s /etc/nginx/sites-available/profiles_api.conf /etc/nginx/sites-enabled/profiles_api.conf
-nginx reload
+# # Configure nginx
+# cp $PROJECT_BASE_PATH/deploy/nginx_profiles_api.conf /etc/nginx/sites-available/profiles_api.conf
+# rm /etc/nginx/sites-enabled/default
+# ln -s /etc/nginx/sites-available/profiles_api.conf /etc/nginx/sites-enabled/profiles_api.conf
+# nginx reload
 
-echo "DONE! :)"
+# echo "DONE! :)"
