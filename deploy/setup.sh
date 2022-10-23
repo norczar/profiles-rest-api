b3c0b3b97afce5cfc847f195b6b3d0674299b129
Norbert Czarnota <czarny-s@o2.pl>
2022-10-23 23:52:14 +0200

    Cleaning project files

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -11,12 +11,6 @@ POSTGRES_NAME=postgres
 POSTGRES_USER=postgres
 POSTGRES_PASSWORD=postgres
 
-# Install Python, SQLite and pip
-# echo "Installing dependencies..."
-# apt-get update
-# apt-get install -y tzdata
-# apt-get install -y python3-dev python3-venv python-pip supervisor nginx git locales
-
 # Create project directory
 mkdir -p $PROJECT_BASE_PATH
 git clone $PROJECT_GIT_URL $PROJECT_BASE_PATH
@@ -26,16 +20,5 @@ cd $PROJECT_BASE_PATH
 python manage.py migrate
 python manage.py collectstatic --noinput
 
-# # Configure supervisor
-# cp $PROJECT_BASE_PATH/deploy/supervisor_profiles_api.conf /etc/supervisor/conf.d/profiles_api.conf
-# supervisorctl reread
-# supervisorctl update
-# supervisorctl restart profiles_api
-
-# # Configure nginx
-# cp $PROJECT_BASE_PATH/deploy/nginx_profiles_api.conf /etc/nginx/sites-available/profiles_api.conf
-# rm /etc/nginx/sites-enabled/default
-# ln -s /etc/nginx/sites-available/profiles_api.conf /etc/nginx/sites-enabled/profiles_api.conf
-
 uwsgi --http :9000 --wsgi-file /usr/local/apps/composeexample/wsgi.py
-# echo "DONE! :)"
+
--- deploy/setup2.sh
+++ deploy/setup2.sh
@@ -14,7 +14,6 @@ POSTGRES_PASSWORD=postgres
 # Install Python, SQLite and pip
 # echo "Installing dependencies..."
 apt-get update
-# apt-get install -y tzdata
 apt-get install -y python3-dev python3-venv pip
 
 # Create project directory
@@ -26,16 +25,8 @@ cd $PROJECT_BASE_PATH
 python3 manage.py migrate
 python3 manage.py collectstatic --noinput
 
-# # Configure supervisor
-# cp $PROJECT_BASE_PATH/deploy/supervisor_profiles_api.conf /etc/supervisor/conf.d/profiles_api.conf
-# supervisorctl reread
-# supervisorctl update
-# supervisorctl restart profiles_api
-
 # # Configure nginx
 cp $PROJECT_BASE_PATH/deploy/nginx_profiles_api.conf /etc/nginx/conf.d/default.conf
 
 nginx
 tail -f /var/log/nginx/access.log
-# uwsgi --http :9000 --wsgi-file /usr/local/apps/composeexample/wsgi.py
-# echo "DONE! :)"
